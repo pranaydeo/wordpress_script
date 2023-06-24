a8f4c34a5595da13fd01d395773d389668d2d07f
Ubuntu <ubuntu@ip-172-31-84-231.ec2.internal>
2023-06-24 11:12:49 +0000

    initial commit

---
+++ wordpress_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check if docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
+
+    # Add commands here to install Docker based on the operating system
+
+    sudo apt-get update
+    sudo apt-get install docker.io -y
+fi
+
+# Check if docker-compose is installed
+
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose is not installed. Installing Docker Compose..."
+
+    # Add commands here to install Docker Compose
+    sudo apt-get update
+    sudo apt-get install docker-compose -y
+fi
+
+# Check if both Docker and Docker Compose are installed
+
+if ! command -v docker &> /dev/null || ! command -v docker-compose &> /dev/null; then
+    echo "Failed to install Docker and/or Docker Compose. Exiting."
+    exit 1
+fi
+
+# Check if the site name argument is provided
+
+if [ $# -ne 1 ]; then
+  echo "Please provide a site name as an argument."
+  exit 1
+fi
